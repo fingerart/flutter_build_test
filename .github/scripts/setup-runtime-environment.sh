ff378015fd279a2eb788f379281fda6104275c59
fingerart <fingerart@sina.com>
2024-05-08 17:54:40 +0800

    Update

--- .github/scripts/setup-runtime-environment.sh
+++ .github/scripts/setup-runtime-environment.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/env bash
 
 # 创建环境变量
-echo "GITHUB_SHA_SHORT=${GITHUB_SHA:0:8}" >> "$GITHUB_ENV"
-echo "REPOSITORY_NAME=${GITHUB_REPOSITORY#*/}" >> "$GITHUB_ENV"
+github_sha_short="${GITHUB_SHA:0:8}"
+repository_name="${GITHUB_REPOSITORY#*/}"
+echo "GITHUB_SHA_SHORT=$github_sha_short" >> "$GITHUB_ENV"
+echo "REPOSITORY_NAME=$repository_name" >> "$GITHUB_ENV"
 
 # 创建输出值
-echo "version_name=$(yq '.version' pubspec.yaml | cut -d'+' -f1)" >> "$GITHUB_OUTPUT"
-echo "version_code=$(yq '.version' pubspec.yaml | cut -d'+' -f2)" >> "$GITHUB_OUTPUT"
+if [ "$GITHUB_REF_NAME" == 'test' ];
+then
+  debug_flag='--debug'
+  output_file_name='app-debug'
+else
+  output_file_name='app-release'
+fi
+version_name="$(yq '.version' pubspec.yaml | cut -d'+' -f1)"
+version_code="$(yq '.version' pubspec.yaml | cut -d'+' -f2)"
+dist_file_name="v$version_name+$version_code $debug_flag $github_sha_short"
+dist_dir="$repository_name/$GITHUB_REF_NAME/v$version_name"
+
+echo "version_name=$version_name" >> "$GITHUB_OUTPUT"
+echo "version_code=$version_code" >> "$GITHUB_OUTPUT"
+echo "dist_file_name=$dist_file_name" >> "$GITHUB_OUTPUT"
+echo "dist_dir=$dist_dir" >> "$GITHUB_OUTPUT"
+echo "output_file_name=$output_file_name" >> "$GITHUB_OUTPUT"
+
+echo "::debug:: $GITHUB_OUTPUT"
