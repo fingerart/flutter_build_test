66a6c6d9ab89e0307d56875cc2b310c9eb681f32
fingerart <fingerart@sina.com>
2024-05-08 19:19:31 +0800

    Update

--- .github/scripts/setup-runtime-environment.sh
+++ .github/scripts/setup-runtime-environment.sh
@@ -3,8 +3,12 @@
 # 创建环境变量
 github_sha_short="${GITHUB_SHA:0:8}"
 repository_name="${GITHUB_REPOSITORY#*/}"
-echo "GITHUB_SHA_SHORT=$github_sha_short" >> "$GITHUB_ENV"
-echo "REPOSITORY_NAME=$repository_name" >> "$GITHUB_ENV"
+project_name="${REPOSITORY_ALIAS:-$repository_name}"
+{
+  echo "GITHUB_SHA_SHORT=$github_sha_short"
+  echo "REPOSITORY_NAME=$repository_name"
+  echo "PROJECT_NAME=$project_name"
+} >> "$GITHUB_ENV"
 
 # 创建输出值
 if [ "$GITHUB_REF_NAME" == 'test' ];
@@ -16,11 +20,13 @@ else
 fi
 version_name="$(yq '.version' pubspec.yaml | cut -d'+' -f1)"
 version_code="$(yq '.version' pubspec.yaml | cut -d'+' -f2)"
-dist_file_name="v$version_name+$version_code$debug_flag($github_sha_short)"
-dist_dir="$repository_name/$GITHUB_REF_NAME/v$version_name"
+dist_file_name="$project_name-v$version_name+$version_code$debug_flag($github_sha_short)"
+dist_dir="$project_name/$GITHUB_REF_NAME/v$version_name"
 
-echo "version_name=$version_name" >> "$GITHUB_OUTPUT"
-echo "version_code=$version_code" >> "$GITHUB_OUTPUT"
-echo "dist_file_name=$dist_file_name" >> "$GITHUB_OUTPUT"
-echo "dist_dir=$dist_dir" >> "$GITHUB_OUTPUT"
-echo "output_file_path=$output_file_path" >> "$GITHUB_OUTPUT"
+{
+  echo "version_name=$version_name"
+  echo "version_code=$version_code"
+  echo "dist_file_name=$dist_file_name"
+  echo "dist_dir=$dist_dir"
+  echo "output_file_path=$output_file_path"
+} >> "$GITHUB_OUTPUT"
